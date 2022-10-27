c1faec8fc16cb133c491455df08a6671af3c6aca
dolores <loliiipineyro@gmail.com>
2022-10-27 19:16:31 -0300

    commiteando el prijecto que me olvide

---
+++ src/data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "balenciaga",
+      name: "Hourglass bag",
+      price: 3500,
+      desc: "With its mix of classic and quirky style and desirable reputation, the Balenciaga Hourglass bag has shaped up to be a legendary It bag.",
+      img: "img/balenciaga.jpg",
+    },
+    {
+      id: "hermes",
+      name: "Birkin bag",
+      price: 16000,
+      desc: "Birkin bags are specially made purses designed with high quality materials, including cow, lizard, and luxurious ostrich, as well as various types of crocodile skin.",
+      img: "img/birkin.jpg",
+    },
+    {
+      id: "chanel",
+      name: "C-55 bag",
+      price: 2500,
+      desc: "A rectangular shape made of quilted leather, with an all-chain strap and the mademoiselle lock, these are the most popular characteristics to identify the original Chanel 2.55 bag. It was designed by Gabrielle Chanel herself in February 1955, at the age of 71.",
+      img: "img/chanel.jpg",
+    },
+    {
+      id: "prada",
+      name: "Cleo bag",
+      price: 4500,
+      desc: "The Prada Cleo bag with sophisticated allure reinterprets an iconic design of the brand from the 1990's. Sleek curved lines emphasized by the particular construction rounded on the bottom and sides give this hobo bag a soft, light look.",
+      img: "img/prada.jpg",
+    },
+  ];
